61d36db21e5e69309a660acf102505fa1d7b7b8a
Pierre Lulé <pierre@lule.fr>
2011-07-07 17:17:25 +0200

    modification arborescence redis

--- redis-interface.rb
+++ redis-interface.rb
@@ -15,9 +15,9 @@ require 'redis/connection/hiredis'
 require 'redis'
 
 PREFIX = "network"
-MULTI  = "multiplexers"
-SENS   = "sensors"
-ACTU   = "actuators"
+MULTI  = "multiplexer"
+SENS   = "sensor"
+ACTU   = "actuator"
 VALUE  = "value"
 CONF   = "config"
 DEL    = "delete"
@@ -33,57 +33,58 @@ class Redis_interface
 	end
 	
 	def get_multi_keys
-		@redis.hkeys("#{@prefix}:#{MULTI}:#{CONF}").collect{|k| k.to_i}
+		@redis.hkeys("#{@prefix}.#{MULTI}.#{CONF}").collect{|k| k.to_i}
 	end
 	
 	def get_multi_config multi_id
-		JSON.parse(@redis.hget("#{@prefix}:#{MULTI}:#{CONF}", multi_id))
+		JSON.parse(@redis.hget("#{@prefix}.#{MULTI}.#{CONF}", multi_id))
 	end
 	
 	def get_multis_config
-		configs = @redis.hgetall("#{@prefix}:#{MULTI}:#{CONF}")
+		configs = @redis.hgetall("#{@prefix}.#{MULTI}.#{CONF}")
 		Hash[*configs.collect{|conf| [conf[0].to_i, JSON.parse(conf[1])]}.flatten]
 	end
 	
 	def set_multi_config multi_id, config
-		path = "#{@prefix}:#{MULTI}"
-		@redis.hset("#{path}:#{CONF}", multi_id, config.to_json)
-		@redis.publish("#{path}:#{multi_id}:#{CONF}", config.to_json)
+		path = "#{@prefix}.#{MULTI}"
+		@redis.hset("#{path}.#{CONF}", multi_id, config.to_json)
+		@redis.publish("#{path}:#{multi_id}.#{CONF}", config.to_json)
 	end
 	
 	def knows_multi? multi_id
-		path = "#{@prefix}:#{MULTI}:#{CONF}"
+		path = "#{@prefix}.#{MULTI}.#{CONF}"
 		@redis.hexists(path, multi_id)
 	end
 	
 	def publish_value(multi_id, sensor, value)
-		path = "#{@prefix}:#{MULTI}:#{multi_id}:#{SENS}"
+		path = "#{@prefix}.#{MULTI}:#{multi_id}.#{SENS}"
 		key = {"value" => value,"timestamp" => Time.now.to_f}.to_json
-		@redis.hset("#{path}:#{VALUE}", sensor, key)
-		@redis.publish("#{path}:#{sensor}:#{VALUE}", value)
+		@redis.hset("#{path}.#{VALUE}", sensor, key)
+		@redis.publish("#{path}:#{sensor}.#{VALUE}", value)
 	end
 	
+	#
 	def set_actuator_value(multi_id, actuator, value)
-		path = "#{@prefix}:#{MULTI}:#{multi_id}:#{ACTU}"
+		path = "#{@prefix}.#{MULTI}:#{multi_id}.#{ACTU}"
 		key = {"value" => value,"timestamp" => Time.now.to_f}.to_json
-		@redis.hset("#{path}:#{VALUE}", actuator, key)
-		@redis.publish("#{path}:#{actuator}:#{VALUE}", value)
+		@redis.hset("#{path}.#{VALUE}", actuator, key)
+		@redis.publish("#{path}:#{actuator}.#{VALUE}", value)
 	end
 	
 	def set_sensor_config multi_id, pin, config
-		path = "#{@prefix}:#{MULTI}:#{multi_id}:#{SENS}"
-		@redis.hset("#{path}:#{CONF}", pin, config.to_json)
-		@redis.publish("#{path}:#{pin}:#{CONF}", config.to_json)
+		path = "#{@prefix}.#{MULTI}:#{multi_id}.#{SENS}"
+		@redis.hset("#{path}.#{CONF}", pin, config.to_json)
+		@redis.publish("#{path}:#{pin}.#{CONF}", config.to_json)
 	end
 	
 	def remove_sensor multi_id, pin
-		path = "#{@prefix}:#{MULTI}:#{multi_id}:#{SENS}"
-		@redis.hdel("#{path}:#{CONF}", pin)
-		@redis.publish("#{path}:#{pin}:#{DEL}", pin)
+		path = "#{@prefix}.#{MULTI}:#{multi_id}.#{SENS}"
+		@redis.hdel("#{path}.#{CONF}", pin)
+		@redis.publish("#{path}:#{pin}.#{DEL}", pin)
 	end
 	
 	def get_sensors_config multi_id #TODO test
-		path = "#{@prefix}:#{MULTI}:#{multi_id}:#{SENS}:#{CONF}"
+		path = "#{@prefix}.#{MULTI}:#{multi_id}.#{SENS}.#{CONF}"
 		ans = {}
 		@redis.hgetall(path).each do |k, v|
 			ans[k.to_i] = JSON.parse(v)
@@ -95,9 +96,9 @@ class Redis_interface
 		Thread.new{
 			type = {:sensor => SENS, :actuator => ACTU}[type]
 			redis = Redis.new
-			redis.psubscribe("#{@prefix}:#{MULTI}:#{multi}:#{type}:#{pin}:value") do |on|
+			redis.psubscribe("#{@prefix}.#{MULTI}:#{multi}.#{type}:#{pin}.value") do |on|
 				on.pmessage do |pattern, channel, valeur|
-					parse = Hash[ *channel.split(":")[0..-2] ]
+					parse = Hash[ *channel.scan(/(\w+):(\w+)/).flatten ]
 					yield parse[MULTI], parse[SENS], valeur
 				end
 			end
@@ -107,9 +108,9 @@ class Redis_interface
 	def on_new_sensor(&block)
 		Thread.new{
 			redis = Redis.new :host => @host, :port => @port
-			redis.psubscribe("#{@prefix}:#{MULTI}:*:#{SENS}:*:#{CONF}") do |on|
+			redis.psubscribe("#{@prefix}.#{MULTI}:*.#{SENS}:*.#{CONF}") do |on|
 				on.pmessage do |pattern, channel, message|
-					parse = Hash[ *channel.split(":")[0..-2] ]
+					parse = Hash[ *channel.scan(/(\w+):(\w+)/).flatten ]
 					yield parse[MULTI], parse[SENS], JSON.parse(message)
 				end
 			end
@@ -119,9 +120,9 @@ class Redis_interface
 	def on_deleted_sensor(&block)
 		Thread.new{
 			redis = Redis.new :host => @host, :port => @port
-			redis.psubscribe("#{@prefix}:#{MULTI}:*:#{SENS}:*:#{DEL}") do |on|
+			redis.psubscribe("#{@prefix}.#{MULTI}:*.#{SENS}:*.#{DEL}") do |on|
 				on.pmessage do |pattern, channel, message|
-					parse = Hash[ *channel.split(":")[0..-2] ]
+					parse = Hash[ *channel.scan(/(\w+):(\w+)/).flatten ]
 					yield parse[MULTI], parse[SENS]
 				end
 			end
